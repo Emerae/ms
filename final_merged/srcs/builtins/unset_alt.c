4983e9fb9723aa4d59195915773d1baab5ce6aea
Emerae <emera42@yahoo.com>
2025-04-22 20:41:58 +0200

    Create unset_alt.c

---
+++ final_merged/srcs/builtins/unset_alt.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_unset.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 12:00:00 by anonymous         #+#    #+#             */
+/*   Updated: 2025/04/22 12:00:00 by anonymous        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+static int	is_valid_env_key(char *key)
+{
+	int	i;
+
+	if (!key || !*key || ft_isdigit(*key))
+		return (0);
+	i = 0;
+	while (key[i])
+	{
+		if (!ft_isalnum(key[i]) && key[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	builtin_unset(char **args, t_data *data)
+{
+	int	i;
+	int	status;
+
+	if (!args[1])
+		return (0);
+	status = 0;
+	i = 1;
+	while (args[i])
+	{
+		if (!is_valid_env_key(args[i]))
+		{
+			print_error("unset", "not a valid identifier", args[i]);
+			status = 1;
+		}
+		else
+			remove_env_var(data->env, args[i]);
+		i++;
+	}
+	return (status);
+}
