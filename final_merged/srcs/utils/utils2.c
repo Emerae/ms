984509b66e81b6f97634889b7e5cb882bb814e30
Emerae <emera42@yahoo.com>
2025-04-22 20:36:07 +0200

    Create utils2.c

---
+++ final_merged/srcs/utils/utils2.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 12:00:00 by anonymous         #+#    #+#             */
+/*   Updated: 2025/04/22 12:00:00 by anonymous        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	free_redirections(t_redir *redirections)
+{
+	t_redir	*tmp;
+
+	while (redirections)
+	{
+		tmp = redirections;
+		redirections = redirections->next;
+		if (tmp->file)
+			free(tmp->file);
+		free(tmp);
+	}
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
