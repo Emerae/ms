e81967ce1b6e55676ac0aa712eb597b24deabfff
Emerae <emera42@yahoo.com>
2025-04-22 18:34:16 +0200

    Create env.c

---
+++ final_merged/srcs/builtins/env.c
@@ -0,0 +1,18 @@
+int	ft_builtin_env(t_env *env_list)
+{
+	t_env	*current;
+
+	current = env_list;
+	while (current)
+	{
+		if (current->value)
+		{
+			ft_putstr_fd(current->key, STDOUT_FILENO);
+			ft_putchar_fd('=', STDOUT_FILENO);
+			ft_putstr_fd(current->value, STDOUT_FILENO);
+			ft_putchar_fd('\n', STDOUT_FILENO);
+		}
+		current = current->next;
+	}
+	return (0);
+}
