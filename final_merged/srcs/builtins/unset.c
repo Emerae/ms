426be91be836943c219a823fc405b9a6b37cd005
Emerae <emera42@yahoo.com>
2025-04-22 18:36:12 +0200

    Create unset.c

---
+++ final_merged/srcs/builtins/unset.c
@@ -0,0 +1,18 @@
+ft_builtin_unset(t_command *cmd, t_env *env_list)
+{
+	int	i;
+
+	i = 1;
+	while (cmd->argv[i])
+	{
+		if (!ft_is_valid_identifier(cmd->argv[i]))
+		{
+			ft_error_msg("unset", "not a valid identifier", 1);
+			i++;
+			continue ;
+		}
+		ft_unsetenv(env_list, cmd->argv[i]);
+		i++;
+	}
+	return (0);
+}
