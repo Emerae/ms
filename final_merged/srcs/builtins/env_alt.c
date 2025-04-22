f916d3d59e975f3b86358ed9ad7599b85447f970
Emerae <emera42@yahoo.com>
2025-04-22 20:41:33 +0200

    Create env_alt.c

---
+++ final_merged/srcs/builtins/env_alt.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_env.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 12:00:00 by anonymous         #+#    #+#             */
+/*   Updated: 2025/04/22 12:00:00 by anonymous        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	builtin_env(t_data *data)
+{
+	t_env	*env;
+
+	env = data->env;
+	while (env)
+	{
+		if (env->value)
+		{
+			ft_putstr_fd(env->key, STDOUT_FILENO);
+			ft_putchar_fd('=', STDOUT_FILENO);
+			ft_putendl_fd(env->value, STDOUT_FILENO);
+		}
+		env = env->next;
+	}
+	return (0);
+}
